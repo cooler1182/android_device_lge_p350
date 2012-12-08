75bfa133975fb7a017b5a422a7520d91cbc414b0
antdking <antohnydking@gmail.com>
2012-12-08 19:05:32 +0000

    fix rill, thank you @yajnab

--- ril/telephony/java/com/android/internal/telephony/LGPecanRIL.java
+++ ril/telephony/java/com/android/internal/telephony/LGPecanRIL.java
@@ -8,13 +8,9 @@ import android.content.Context;
 import android.os.Message;
 import android.os.Parcel;
 
-import com.android.internal.telephony.gsm.NetworkInfo;
+import com.android.internal.telephony.cdma.CdmaInformationRecords;
 
 public class LGPecanRIL extends RIL implements CommandsInterface {
-
-	public LGPecanRIL(Context context) {
-		super(context);
-	}
 
     public LGPecanRIL(Context context, int networkMode, int cdmaSubscription) {
         super(context, networkMode, cdmaSubscription);
@@ -38,20 +34,20 @@ public class LGPecanRIL extends RIL implements CommandsInterface {
 
     @Override
     protected Object
-    responseNetworkInfos(Parcel p) {
+    responseOperatorInfos(Parcel p) {
         String strings[] = (String [])responseStrings(p);
 
         if (strings.length % 5 != 0) {
             throw new RuntimeException(
                 "RIL_REQUEST_QUERY_AVAILABLE_NETWORKS: invalid response. Got "
-                + strings.length + " strings, expected multiple of 5");
+                + strings.length + " strings, expected multible of 5");
         }
 
-        ArrayList<NetworkInfo> ret = new ArrayList<NetworkInfo>(strings.length / 5);
+        ArrayList<OperatorInfo> ret = new ArrayList<OperatorInfo>(strings.length / 5);
 
         for (int i = 0 ; i < strings.length ; i += 5) {
             ret.add (
-                new NetworkInfo(
+                new OperatorInfo(
                     strings[i+0],
                     strings[i+1],
                     strings[i+2],
