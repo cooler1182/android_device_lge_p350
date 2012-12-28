faf864d080d1722c31d9ed699cee45dcf84d219d
RonGokhale <cips173@gmail.com>
2012-12-28 12:42:50 +0530

    p350: cleanup
    Change-Id: Iae7033fd9fe737300040a6af1815c58759be20fe

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,26 +1,23 @@
+USE_CAMERA_STUB := false
+
 # inherit from the proprietary version
 -include vendor/lge/p350/BoardConfigVendor.mk
 
-# Camera
-USE_CAMERA_STUB := false
-BOARD_USE_NASTY_PTHREAD_CREATE_HACK := true
-COMMON_GLOBAL_CFLAGS += -DBINDER_COMPAT
-
-# Platform
+# Platform & cpu
+TARGET_CPU_ABI := armeabi-v6l
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv6-vfp
+ARCH_ARM_HAVE_VFP := true
 TARGET_NO_INITLOGO := true
 TARGET_NO_BOOTLOADER := true
 TARGET_BOOTLOADER_BOARD_NAME := p350
 TARGET_OTA_ASSERT_DEVICE := pecan,p350,p355
 TARGET_BOARD_PLATFORM := msm7x27
-TARGET_BOOTANIMATION_PRELOAD := true
-TARGET_BOOTANIMATION_TEXTURE_CACHE := true
 BOARD_HAS_NO_SELECT_BUTTON := true
 
-# CPU
-TARGET_CPU_ABI := armeabi-v6l
-TARGET_CPU_ABI2 := armeabi
-TARGET_ARCH_VARIANT := armv6-vfp
-ARCH_ARM_HAVE_VFP := true
+# Camera
+BOARD_USE_NASTY_PTHREAD_CREATE_HACK := true
+COMMON_GLOBAL_CFLAGS += -DBINDER_COMPAT
 
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/lge/p350
@@ -30,14 +27,6 @@ BOARD_KERNEL_CMDLINE := mem=215M console=ttyMSM2,115200n8 androidboot.hardware=p
 BOARD_KERNEL_BASE := 0x02808000
 BOARD_KERNEL_PAGESIZE := 2048
 
-# Browser & WebKit
-JS_ENGINE := v8
-HTTP := chrome
-WITH_JIT := true
-ENABLE_JSC_JIT := true
-ENABLE_WEBGL := true
-TARGET_FORCE_CPU_UPLOAD := true
-
 # Graphic
 USE_OPENGL_RENDERER := true
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
@@ -45,12 +34,11 @@ TARGET_USE_OVERLAY := false
 TARGET_HAVE_BYPASS := false
 TARGET_USES_C2D_COMPOSITION := false
 TARGET_USES_GENLOCK := true
-BOARD_USES_QCOM_LIBS := true
-BOARD_USES_QCOM_HARDWARE := true
 BOARD_USE_SKIA_LCDTEXT := true
 BOARD_ADRENO_DECIDE_TEXTURE_TARGET := true
 BOARD_EGL_CFG := device/lge/p350/configs/egl.cfg
 
+# cflag
 COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
 COMMON_GLOBAL_CFLAGS += -DTARGET_MSM7x27
 COMMON_GLOBAL_CFLAGS += -DREFRESH_RATE=60
@@ -63,9 +51,6 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x0c800000
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x0bd80000
 BOARD_FLASH_BLOCK_SIZE := 131072
 
-# Small system
-BOARD_HAS_SMALL_SYSTEM_PARTITION := true
-
 # Sensors
 TARGET_USES_OLD_LIBSENSORS_HAL := true
 
@@ -75,9 +60,9 @@ BOARD_HAS_JANKY_BACKBUFFER := true
 BOARD_NO_RGBX_8888 := true
 BOARD_USE_CUSTOM_RECOVERY_FONT := '"font_7x16.h"'
 
-## Wi-Fi Hotspot
-BOARD_HAVE_LEGACY_HOSTAPD := true
-BOARD_HOSTAPD_NO_ENTROPY := true
+# BOOTANIMATION
+TARGET_BOOTANIMATION_PRELOAD := true
+TARGET_BOOTANIMATION_TEXTURE_CACHE := true
 
 # Audio and Bluetooth
 TARGET_PROVIDES_LIBAUDIO := true
@@ -112,17 +97,24 @@ TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun0/f
 BOARD_UMS_LUNFILE := "/sys/devices/platform/msm_hsusb/gadget/lun0/file"
 
 # Wi-Fi & Wi-Fi HotSpot
-WPA_SUPPLICANT_VERSION := VER_0_6_X
-BOARD_WLAN_DEVICE := bcm4329
-BOARD_WEXT_NO_COMBO_SCAN := true
-BOARD_WPA_SUPPLICANT_DRIVER := WEXT
-WIFI_DRIVER_HAS_LGE_SOFTAP := true
-WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/wireless.ko"
-WIFI_DRIVER_MODULE_ARG := "firmware_path=/etc/wl/rtecdc.bin nvram_path=/etc/wl/nvram.txt config_path=/data/misc/wifi/config"
-WIFI_DRIVER_MODULE_NAME := "wireless"
-WIFI_DRIVER_FW_PATH_STA := "/system/etc/wl/rtecdc.bin"
-WIFI_DRIVER_FW_PATH_AP := "/system/etc/wl/rtecdc-apsta.bin"
-BOARD_HAVE_LGE_WIFI := true
+WPA_SUPPLICANT_VERSION          := VER_0_8_X
+BOARD_WLAN_DEVICE               := bcm4329
+BOARD_WEXT_NO_COMBO_SCAN        := true
+BOARD_WPA_SUPPLICANT_DRIVER     := WEXT
+WIFI_DRIVER_HAS_LGE_SOFTAP      := true
+WIFI_DRIVER_MODULE_PATH         := "/system/lib/modules/wireless.ko"
+WIFI_DRIVER_MODULE_ARG          := "firmware_path=/etc/wl/rtecdc.bin nvram_path=/etc/wl/nvram.txt config_path=/data/misc/wifi/config"
+WIFI_DRIVER_MODULE_NAME         := "wireless"
+WIFI_DRIVER_FW_PATH_STA         := "/system/etc/wl/rtecdc.bin"
+WIFI_DRIVER_FW_PATH_AP          := "/system/etc/wl/rtecdc-apsta.bin"
+
+# Browser & WebKit
+JS_ENGINE := v8
+HTTP := chrome
+WITH_JIT := true
+ENABLE_JSC_JIT := true
+ENABLE_WEBGL := true
+TARGET_FORCE_CPU_UPLOAD := true
 
 # Touch screen compatibility for ICS
 BOARD_USE_LEGACY_TOUCHSCREEN := true
